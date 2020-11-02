0ae7ac3b6a80ced296de0bceae4cca8461a4c6cc
Piotr Daniel <p.daniel.london@gmail.com>
2020-11-02 17:14:20 +0000

    more media query adjustments

--- main.js
+++ main.js
@@ -16,38 +16,55 @@ userInput.focus();
 
 // adds users dishes to the list
 const adding = () => {
-    userTypedText = userInput.value;
-    answerDish.innerHTML = `You have added <strong><u>${userTypedText}</u></strong> to the list of your favourite dinner ideas.`;
-    userDishes.push(userTypedText);
-    localStorage.setItem("savedFood", JSON.stringify(userDishes));
-    userInput.value = "";
-    userInput.focus();
+    if (userInput.value == 0) {
+        answerDish.style.backgroundColor = "red";
+        answerDish.style.color = "white";
+        answerDish.innerHTML = `Please type in your dish name.`;
+        userInput.value = "";
+        userInput.focus();
+    } else {
+        answerDish.style.backgroundColor = "teal";
+        answerDish.style.color = "white";
+        userTypedText = userInput.value;
+        answerDish.innerHTML = `You have added <strong><u>${userTypedText}</u></strong> to the list of your favourite dinner ideas.`;
+        userDishes.push(userTypedText);
+        localStorage.setItem("savedFood", JSON.stringify(userDishes));
+        userInput.value = "";
+        userInput.focus();
+    }
 }
 
 // dish generator function
 const dishGenerator = () => {
     var retrievedData = localStorage.getItem("savedFood");
     userDishes = JSON.parse(retrievedData);
-    var choicedDish = userDishes[Math.floor(Math.random() * userDishes.length)];
     var choicedStockDish = stockDishes[Math.floor(Math.random() * stockDishes.length)];
-    // checks if used recently
-    var n = recentDishes.includes(choicedDish);
-    if (n == false){
-        // if none entered it will use stock dishes
-        if (userDishes.length == 0) {
-            answerDish.innerHTML = `Your dinner suggestion for today is <strong><u>${choicedStockDish}.</u></strong>`;
-        }
-        // checks if recent limit reached
-        else if (recentDishes.length < 5) {
-            recentDishes.push(choicedDish);
-            answerDish.innerHTML = `Your dinner suggestion for today is <strong><u>${choicedDish}.</u></strong>`;
-        } else {
-            recentDishes.shift();
-            recentDishes.push(choicedDish);
-            answerDish.innerHTML = `Your dinner suggestion for today is <strong><u>${choicedDish}.</u></strong>`;
-        }
+    // if no dishes entered it will use stock dishes
+    if (userDishes.length == 0) {
+        answerDish.style.backgroundColor = "teal";
+        answerDish.style.color = "white";
+        answerDish.innerHTML = `Your dinner suggestion for today is: <br><strong><u>${choicedStockDish}!</u></strong>`;
     } else {
-        dishGenerator();
+        var choicedDish = userDishes[Math.floor(Math.random() * userDishes.length)];
+        // checks if choice was used recently and if not, do the function, otherwise repeat
+        var n = recentDishes.includes(choicedDish);
+        if (n == false){
+            // checks if recent dishes limit reached and adds on if not or delete oldest and add new if it did reach
+            if (recentDishes.length < 5) {
+                recentDishes.push(choicedDish);
+                answerDish.style.backgroundColor = "teal";
+                answerDish.style.color = "white";
+                answerDish.innerHTML = `Your dinner suggestion for today is: <br><strong><u>${choicedDish}!</u></strong>`;
+            } else {
+                recentDishes.shift();
+                recentDishes.push(choicedDish);
+                answerDish.style.backgroundColor = "teal";
+                answerDish.style.color = "white";
+                answerDish.innerHTML = `Your dinner suggestion for today is: <br><strong><u>${choicedDish}!</u></strong>`;
+            }
+        } else {
+            dishGenerator();
+        }
     }
 }
 
